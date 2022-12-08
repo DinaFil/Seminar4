19d6cf66cc209b2781c2e83a221e22ca4b3ed1ed
Dina Filippova <dinafilippova@gmail.com>
2022-12-08 15:23:19 +0700

    Длина чисел

---
+++ Задача02/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("input number");
+int number = Convert.ToInt32(Console.ReadLine());
+int count = 0;
+if (number == 0)
+{
+    Console.WriteLine("1");
+}
+else
+{while (number > 0)
+{
+number /= 10;
+count++;
+}
+Console.WriteLine(count);
+}
