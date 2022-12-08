62eef5355bdb924b2152dbd7eaf1e05b66c95c59
Dina Filippova <dinafilippova@gmail.com>
2022-12-08 17:42:48 +0700

    Возведение в степень и факториал Функции

---
+++ Задача03/Program.cs
@@ -0,0 +1,34 @@
+﻿Console.Clear();
+Console.WriteLine("input number");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int Proizv(int M)
+{
+int pz=1;
+while (1 != M)
+{
+pz = pz * M;
+M--;
+}
+return pz;
+}
+Console.WriteLine(Proizv(number));
+
+
+// Функция возведения в степень
+// double Pow(int number, int a) 
+// {
+//     int result = 1;
+//     for (int i = 0; i < a; i++)
+//     {
+//         result *= number;
+//     }
+//     return result;
+//     }
+
+// Console.WriteLine(Pow(3,4))
+
+
+
+
+
---
+++ Задача04/Program.cs
@@ -0,0 +1,14 @@
+﻿void FillArray(int[] collection)
+
+{
+int length = collection.Length;
+int index = 0;
+while (index < length)
+{
+
+index++;
+}
+
+}
+
+int array [] = new int [10]; //создать новый массив, имеющий 10 элементов, по умолчанию, наполненный нулями.
