73d57bb26fc5d720966707708a1b475626d50aea
Dina Filippova <dinafilippova@gmail.com>
2022-12-08 11:22:11 +0700

    Функция Сумма всех чисел от 1 до Н

---
+++ Задача01/Program.cs
@@ -0,0 +1,24 @@
+﻿// int sum = 0;
+// if (number < 1) Console.WriteLine("Введено некорректное число");
+// else
+// {
+// for (int i = 1; i <=number; i++)
+// {
+// sum +=i;
+// }
+// }
+// Console.WriteLine(sum);
+
+Console.WriteLine("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(SumNumerals(number));
+
+int SumNumerals(int numbers)
+{
+int sum = 0;
+for (int i = 1; i <=number; i++)
+{
+sum +=i;
+}
+return sum;
+}
